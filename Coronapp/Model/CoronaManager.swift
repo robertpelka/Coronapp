261b8f0c34f9a6bf16c21e034a4a62a9d5382612
Robert Pelka <123455ff@gmail.com>
2020-10-07 23:49:35 +0200

    MVC pattern

---
+++ Coronapp/Model/CoronaManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoronaManager.swift
+//  Coronapp
+//
+//  Created by Robert Pelka on 07/10/2020.
+//
+
+import Foundation
+
+struct CoronaManager {
+    let url = "https://api.covid19api.com/country/"
+    //poland?from=2020-03-01T00:00:00Z&to=2020-10-10T00:00:00Z
+}
