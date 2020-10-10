789eb06e9a05f314ab09880aff9cab701d5e43fd
Robert Pelka <123455ff@gmail.com>
2020-10-10 23:25:12 +0200

    Added user country detecting.

--- Coronapp/Controller/ViewController.swift
+++ Coronapp/Controller/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
-
-
+    
 }
 
+//MARK: - CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            CLGeocoder().reverseGeocodeLocation(location) { (placeMark, error) in
+                if error != nil {
+                    print(error!)
+                }
+                else {
+                    if let place = placeMark?[0]{
+                        print(place.isoCountryCode!)
+                    }
+                }
+            }
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
---
+++ Coronapp/Model/CoronaData.swift
@@ -0,0 +1,29 @@
+//
+//  CoronaData.swift
+//  Coronapp
+//
+//  Created by Robert Pelka on 10/10/2020.
+//
+
+import Foundation
+
+struct CoronaData: Decodable {
+    let global: Global
+    let countries: [Countries]
+}
+
+struct Global: Decodable {
+    let NewConfirmed: Int
+    let TotalConfirmed: Int
+    let NewDeaths: Int
+    let TotalDeaths: Int
+}
+
+struct Countries: Decodable {
+    let CountryCode: String
+    let NewConfirmed: Int
+    let TotalConfirmed: Int
+    let NewDeaths: Int
+    let TotalDeaths: Int
+    let Date: String
+}
